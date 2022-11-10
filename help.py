14c5843f20849df4c14159474c2bd71dbe28ed77
H-a-z-i <116266630+H-a-z-i@users.noreply.github.com>
2022-11-10 09:15:03 +0000

    Delete help.py

--- help.py
+++
@@ -1,9 +0,0 @@
-
-#The following list of commands are available, all commmands must be run from the server, passing multiple arguments is not supported at this time:
-Command list:
-    -gf:    Get file from client
-    -sf:    Send file to client
-    -shell: Run shell for operating system
-    -exit:  Exit the program or close a connection
-    -h:     Show this help message
-    -l:     list of commands in short hand
